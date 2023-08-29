0cddbbca3a0f8396440fa9bbe039af4feb3bed03
KangMinju <demo1208@gmail.com>
2023-08-29 14:06:58 +0900

    up

--- accounts/views.py
+++ accounts/views.py
@@ -3,13 +3,22 @@ from .forms import CustomUserCreationForm, CustomAuthenticationForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 
+
 # Create your views here.
+
 def signup(request):
+    if request.user.is_authenticated:
+        # 로그인을 한 상태에서는 index 로 가도록
+        return redirect('article:index')
+
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
+        # 여기에서의 form은 인스턴스화가 되었긴 하지만 아직까지 modelform 클래스인 것.
         if form.is_valid():
-            form.save()
-            return redirect('accounts:signup')
+            user = form.save() # form 을 save를 해야만 그 안의 데이터들이 밖으로 나오게 됨.
+            # user에 form.save()한 인스턴스가 할당됨(User클래스의(결국 AbstractUser클래스의) column 데이터들이 할당됨.
+            auth_login(request, user)
+            return redirect('accounts:login')
 
             # 좋은 UX를 생각해보기(회원가입 후 어떤 창으로 이동되는게 좋은지
 
@@ -20,7 +29,7 @@ def signup(request):
         'form': form,
     }
 
-    return render(request, 'signup.html', context) 
+    return render(request, 'account_form.html', context) 
     # 저번 플젝과 바뀐 점은 다른 class를 상속받아서 사용한 것 뿐
 
 def login(request):
@@ -33,7 +42,7 @@ def login(request):
             next_url = request.GET.get('next')
 
 
-            return redirect(next_url or 'accounts:index')
+            return redirect(next_url or 'articles:index')
             # -> 단축평가가 적용됨
             # next 인자가 url에 있을 때 => '/articles/create/'(=T, 어떤 값이 있으니깐) or 'articles:index'
             # next 인자가 url에 없을 때 => None(F)or 'articles:index' 이므로 뒤에있는 값을 사용
@@ -48,7 +57,7 @@ def login(request):
         'form': form,
 
     }
-    return render(request, 'login.html', context)
+    return render(request, 'account_form.html', context)
     # 이때 signup.html 과 login.html 은 완전히 똑같은 구조
     # 따라서 나중에 form.html로 같이 쓰는 거 연습해보기
 def logout(request):
--- articles/forms.py
+++ articles/forms.py
@@ -1,12 +1,28 @@
 from django import forms
-from .models import Article
+from .models import Article, Comment
 
 # ModelForm 은 model 을 알려주기만 하면 자동으로 form을 만들어줌
 
 class ArticleForm(forms.ModelForm):
+    title = forms.CharField(
+        label = 'Title',
+        widget=forms.TextInput(
+            attrs={'class': 'form-control'}
+        )
+    )
+    
     
     class Meta:
         model = Article
         # fields = '__all__'
         # fields = ('title', 'content',)
         exclude = ('user',)
+        # widgets = {
+        #     'title': forms.TextInput(attrs={'class': 'form-contrl'}),
+        #     'content': forms.Textarea(attrs={'class': 'form-control'})
+        # }
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        fields = ('content',)
